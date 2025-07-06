e399850fd380a2a4b57ec6f361735a998e3635ab
leotorresval92 <leotorresval92@gmail.com>
2025-07-06 14:39:04 -0500

    Modificacion ejercicio2tarea2

--- TAREA2_SPARK/Ejercicio2/CategoriaDeVideosMasVista.py
+++ TAREA2_SPARK/Ejercicio2/CategoriaDeVideosMasVista.py
@@ -6,12 +6,12 @@ entrada = sys.argv[1]
 salida = sys.argv[2]
 archivos = [os.path.join(entrada, f) for f in os.listdir(entrada) if f != "log.txt"]
 rdd = spark.sparkContext.textFile(",".join(archivos))
-rdd = rdd.repartition(2)
 rdd = rdd.map(lambda x: x.strip().split("\t"))
 rddCompleto = rdd.filter(lambda x : len(x)>=6)
 rdd1 = rddCompleto.map(lambda x: (x[3].lower(),int(x[5])))
 rdd1 = rdd1.reduceByKey(lambda x,y: x+y)
 rdd1 = rdd1.sortBy(lambda x : x[1],ascending=False)
 categoriaMaxima = rdd1.take(1)
-
-rdd1.saveAsTextFile("salida_"+salida)
+df = spark.createDataFrame(categoriaMaxima, ["categoria", "totalVistas"])
+df_txt = df.selectExpr("concat(categoria, ';', totalVistas) as linea")
+df_txt.write.mode("overwrite").text("salida_"+salida+"/")
